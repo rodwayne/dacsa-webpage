9d3005337d30e13d0faed153b09f1ff24fb8ae58
rodwayne <ricardo260898@gmail.com>
2023-05-25 02:17:03 -0500

    Rename function name

--- js/confirmForm.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("contact-form");
-
-form.addEventListener("submit", confirmationMessage);
-
-const confirmationMessage = () => {
-  alert("La información ha sido enviado con éxito");
-};
